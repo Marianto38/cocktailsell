e263d2ef957e8333ab4150b7652274e0dc6362a9
Maria Antonieta Pereira <antonietap0@gmail.com>
2023-02-27 17:59:57 -0500

    add controler to carousel

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,23 @@
 import { Application } from "@hotwired/stimulus"
 
+// import Carousel from 'stimulus-carousel'
+
+
 const application = Application.start()
+// application.register("carousel", Carousel)
+
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+
+// app/javascript/controllers/index.js
+
+// import { Application } from "stimulus"
+// import Carousel from "stimulus-carousel"
+
+// const application = Application.start()
+// application.register("carousel", Carousel)
---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,15 @@
+// import { Controller } from "@hotwired/stimulus";
+
+// // Connects to data-controller="carousel"
+// export default class extends Controller {
+//   connect() {
+//     super.connect();
+//     console.log('Do what you want here.');
+// }
+// }
+//   //   // The swiper instance.
+//   //   this.swiper;
+
+//   //   // Default options for every carousels.
+//   //   this.defaultOptions;
+//   // }
